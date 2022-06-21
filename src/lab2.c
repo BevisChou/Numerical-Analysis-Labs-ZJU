4b5f1d327086478a6a3ba9bc3cebc5e3f9fb8bb3
Zheng Zhou <bevischou@outlook.com>
2022-06-21 09:50:59 +0800

    Lab 2 fifth attempt
    Finished lab 2 code. 4/6 tests passed on PTA. Case 7 in the comments failed.

--- src/lab2.c
+++ src/lab2.c
@@ -42,6 +42,15 @@ int cmp(const void *lhs, const void *rhs)
     return 0;
 }
 
+void sample(int n, double c[], double a, double h, int idx[])
+{
+    for (int i = 0; i <= N; i++) {
+        idx[i] = i;
+        fs[i] = fabs(calc(n, c, a + i * h));
+    }
+    qsort(idx, N + 1, sizeof(int), cmp);
+}
+
 double calc(int n, double c[], double x)
 {
     double res = 0;
@@ -59,49 +68,88 @@ void derivative(int n, double c[], double res[])
     }
 }
 
-int newton(int n, double c[], double dc[], double x, double *res)
+int newton(int n, double c[], double d1c[], double d2c[], double x, double *res, double EPS)
 {
-    double temp = calc(n - 1, dc, x);
+    double d0 = calc(n, c, x), d1 = calc(n - 1, d1c, x), temp = d1 * d1 - d0 * calc(n - 2, d2c, x);
     if (fabs(temp) < ZERO) {
         return 1;
     }
-    *res = x - calc(n, c, x) / temp;
-    return 0;
+    *res = x - d0 * d1 / temp;
+    if (fabs(d0 / d1) < EPS) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
 }
 
-double trial(int n, double c[], double dc[], double x, double EPS)
+int trial(int n, double c[], double d1c[], double d2c[], double *x, double EPS)
 {
     double ps[3], d0, d1;
-    ps[0] = x;
+    ps[0] = *x;
     // printf("---\nx0: %e\n", ps[0]);
     for (int i = 0; i < MAXI; i++) {
         if (fabs(calc(n, c, ps[0])) < ZERO) {
-            return ps[0];
+            *x = ps[0];
+            // printf("ps[0] - f(%lf) = %lf\n", ps[0], calc(n, c, ps[0]));
+            return 0;
         }
-        if (newton(n, c, dc, ps[0], &ps[1]) != 0 || newton(n, c, dc, ps[1], &ps[2]) != 0) {
-            return ps[0];
+        // printf("before newton 1\n");
+        switch (newton(n, c, d1c, d2c, ps[0], &ps[1], EPS)) {
+            case 1:
+                // printf("newton 1 failed\n");
+                return 1;
+            case -1:
+                *x = ps[1];
+                return 0;
+            default:
+                if (fabs(calc(n, c, ps[1])) < ZERO) {
+                    *x = ps[1];
+                    return 0;
+                }
+        }
+        // printf("ps[1]: %lf\n", ps[1]);
+        switch (newton(n, c, d1c, d2c, ps[1], &ps[2], EPS)) {
+            case 1:
+                // printf("newton 2 failed\n");
+                return 1;
+            case -1:
+                *x = ps[2];
+                return 0;
+            default:
+                if (fabs(calc(n, c, ps[2])) < ZERO) {
+                    *x = ps[2];
+                    return 0;
+                }
         }
         d0 = ps[1] - ps[0];
         d1 = ps[2] - ps[1];
+        if (fabs(d1 - d0) < ZERO) {
+            // printf("ps[2]: %lf - to fail\n", ps[2]);
+            return 1;
+        }
         ps[0] = ps[0] - d0 * d0 / (d1 - d0);
         // printf("iter %d: %e\n", i, ps[0]);
     }
-    return ps[0];
+    return 1;
 }
 
 double Polynomial_Root(int n, double c[], double a, double b, double EPS)
 {
     int idx[N + 1];
-    double h, dc[MAXN - 1], res, xs[N + 1];
+    double h, d1c[MAXN - 1], d2c[MAXN - 2], res;
     h = (b - a) / N;
-    derivative(n, c, dc);
+    sample(n, c, a, h, idx);
+    derivative(n, c, d1c);
+    derivative(n - 1, d1c, d2c);
     for (int i = 0; i <= N; i++) {
-        idx[i] = i;
-        xs[i] = trial(n, c, dc, a + i * h, EPS);
-        fs[i] = fabs(calc(n, c, xs[i]));
+        res = a + idx[i] * h;
+        if (trial(n, c, d1c, d2c, &res, EPS) == 0) {
+            return res;
+        }
     }
-    qsort(idx, N + 1, sizeof(int), cmp);
-    return xs[idx[0]];
+    // printf("failed\n");
+    return 0;
 }
 
 /*
@@ -116,5 +164,21 @@ case 2:
 case 3:
 4 1 0 0 0 0
 -1000000 1000000
+
+case 4:
+3 1 4 0 -10
+1 2
+
+case 5:
+10 1 -10 45 -120 210 -252 210 -120 45 -10 1
+-1000000 1000000
+
+case 6:
+9 1 -9 36 -84 126 -126 84 -36 9 -1
+-1000000 1000000
+
+case 7:
+8 1 -12 62 -180 321 -360 248 -96 16
+-1000000 1.5
 
 */
